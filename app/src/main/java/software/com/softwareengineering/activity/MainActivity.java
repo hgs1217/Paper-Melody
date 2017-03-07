07c4bae1bac006ec993ddab825555228c373dd45
hgs1217 <mf20080540@126.com>
2017-03-07 23:23:33 +0800

    create repository

---
+++ app/src/main/java/software/com/softwareengineering/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package software.com.softwareengineering.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import software.com.softwareengineering.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

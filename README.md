60b83fc5dbff2cd0616e6d3af4803fe108995e03
hgs1217 <mf20080540@126.com>
2017-03-13 23:14:21 +0800

    opencv库的配置
    -配置了opencv库，并编写了导入流程文件
    -把MainActivity修改为一个样例文件供测试opencv库导入是否成功

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Project of Software Engineering
 ## High Up!
 请大家先把项目获取至本地，导入Android Studio进行配置，Gradle版本2.14.1，配置时如有问题请在群里交流
-项目名称在确认内容后可能会修改，届时大家可能需重新pull至本地
+
+由于opencv库非常大，因此我没有上传github，然后我在/docs目录下放置了一个opencv库导入方法，你们按照那个流程导入一下
--- app/src/main/java/com/papermelody/activity/MainActivity.java
+++ app/src/main/java/com/papermelody/activity/MainActivity.java
@@ -1,15 +1,59 @@
 package com.papermelody.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.papermelody.R;
 
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
 public class MainActivity extends AppCompatActivity {
+
+    static{ System.loadLibrary("opencv_java3"); }
+
+    int i=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        final Bitmap bitmap = ((BitmapDrawable) getResources().getDrawable(R.drawable.pyj)).getBitmap();
+        imageView.setImageBitmap(bitmap);
+
+
+        final Button button2 = (Button)findViewById(R.id.button);
+        button2.setText("转换");
+        button2.setOnClickListener(new Button.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                i++;
+                Mat rgbMat = new Mat();
+                Mat grayMat = new Mat();
+                //获取lena彩色图像所对应的像素数据
+                Utils.bitmapToMat(bitmap, rgbMat);
+                //将彩色图像数据转换为灰度图像数据并存储到grayMat中
+                Imgproc.cvtColor(rgbMat, grayMat, Imgproc.COLOR_RGB2GRAY);
+                //创建一个灰度图像
+                Bitmap grayBmp = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.RGB_565);
+                //将矩阵grayMat转换为灰度图像
+                Utils.matToBitmap(grayMat, grayBmp);
+                ImageView imageView = (ImageView)findViewById(R.id.imageView);
+                if(i%2==1)
+                    imageView.setImageBitmap(grayBmp);
+                else
+                    imageView.setImageBitmap(bitmap);
+            }
+
+        });
     }
 }

c6f4c52e939d3efa99dd5616fb686569c573533a
hgs1217 <mf20080540@126.com>
2017-03-19 15:58:16 +0800

    导入钢琴音效
    -导入钢琴88键音效
    -首页放置了音效播放的样例
    -ButterKnife库的导入调整，现在使用BindView只需用R资源库而不用转化为R2了

--- app/src/main/java/com/papermelody/activity/MainActivity.java
+++ app/src/main/java/com/papermelody/activity/MainActivity.java
@@ -2,13 +2,15 @@ package com.papermelody.activity;
 
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.papermelody.R;
-import com.papermelody.R2;
 
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
@@ -21,24 +23,46 @@ public class MainActivity extends BaseActivity {
      * 主页面
      * 当前页面供导入opencv包时测试使用，之后的版本需要将整个页面重做
      * 这个页面现在的一些写法，例如ButterKnife和lambda写法的使用，可作将来代码的模板风格参考
+     * 同时首页放入了音效播放的实现，可供参考
      */
 
-    @BindView(R2.id.imageView)
-    ImageView imageView;
-    @BindView(R2.id.button)
-    Button button2;
+    @BindView(R.id.image_img)
+    ImageView imageImg;
+    @BindView(R.id.button_switch)
+    Button buttonSwitch;
+    @BindView(R.id.button_c4)
+    Button buttonC4;
+    @BindView(R.id.button_d4)
+    Button buttonD4;
+    @BindView(R.id.button_e4)
+    Button buttonE4;
+    @BindView(R.id.button_f4)
+    Button buttonF4;
+    @BindView(R.id.button_g4)
+    Button buttonG4;
+    @BindView(R.id.button_a4)
+    Button buttonA4;
+    @BindView(R.id.button_b4)
+    Button buttonB4;
 
+    private int[] voiceResId = new int [] {R.raw.c4, R.raw.d4, R.raw.e4, R.raw.f4, R.raw.g4, R.raw.a4, R.raw.b4};
+    private String[] buttonString = new String [] {"C4", "D4", "E4", "F4", "G4", "A4", "B4"};
+
+    private SoundPool mSoundPool;
+    private int[] voiceId = new int [7];
     private int i=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        final Bitmap bitmap = ((BitmapDrawable) getResources().getDrawable(R.drawable.pyj)).getBitmap();
-        imageView.setImageBitmap(bitmap);
+        Button[] buttonSound = new Button [] {buttonC4, buttonD4, buttonE4, buttonF4, buttonG4, buttonA4, buttonB4};
 
-        button2.setText("转换");
-        button2.setOnClickListener((View v) -> {
+        final Bitmap bitmap = ((BitmapDrawable) getResources().getDrawable(R.drawable.pyj)).getBitmap();
+        imageImg.setImageBitmap(bitmap);
+
+        buttonSwitch.setText("转换");
+        buttonSwitch.setOnClickListener((View v) -> {
                 i++;
                 Mat rgbMat = new Mat();
                 Mat grayMat = new Mat();
@@ -50,12 +74,27 @@ public class MainActivity extends BaseActivity {
                 Bitmap grayBmp = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.RGB_565);
                 //将矩阵grayMat转换为灰度图像
                 Utils.matToBitmap(grayMat, grayBmp);
-                ImageView imageView = (ImageView)findViewById(R.id.imageView);
                 if(i%2==1)
-                    imageView.setImageBitmap(grayBmp);
+                    imageImg.setImageBitmap(grayBmp);
                 else
-                    imageView.setImageBitmap(bitmap);
+                    imageImg.setImageBitmap(bitmap);
             });
+
+        SoundPool.Builder spb = new SoundPool.Builder();
+        spb.setMaxStreams(10);
+        AudioAttributes.Builder attrBuilder = new AudioAttributes.Builder();
+        attrBuilder.setLegacyStreamType(AudioManager.STREAM_MUSIC);
+        spb.setAudioAttributes(attrBuilder.build());
+        mSoundPool = spb.build();
+
+        for (int i=0; i<voiceId.length; ++i) {
+            voiceId[i] = mSoundPool.load(this, voiceResId[i], 1);
+            buttonSound[i].setText(buttonString[i]);
+            final int fi = i;
+            buttonSound[i].setOnClickListener((View v) -> {
+                mSoundPool.play(voiceId[fi], 1, 1, 0, 0, 1);
+            });
+        }
     }
 
     @Override

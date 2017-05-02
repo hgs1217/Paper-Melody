ab1e770cab1bea9b6ef24890f74ebf427208bd24
unknown <1329575336@qq.com>
2017-05-02 20:29:18 +0800

    在PlayActivity中显示出设置的选项； 留有一个诡异的bug

--- app/src/main/java/com/papermelody/activity/PlayActivity.java
+++ app/src/main/java/com/papermelody/activity/PlayActivity.java
@@ -1,9 +1,16 @@
 package com.papermelody.activity;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.papermelody.R;
+
+import butterknife.BindInt;
+import butterknife.BindView;
 
 /**
  * Created by HgS_1217_ on 2017/3/18.
@@ -14,17 +21,43 @@ public class PlayActivity extends BaseActivity {
      * 用例：演奏乐器
      * 弹奏乐器时的界面，可能存在虚拟乐器或曲谱等内容
      */
+    @BindView(R.id.text_mode)
+    TextView textViewMode;
+    @BindView(R.id.text_opern)
+    TextView textViewOpern;
+    @BindView(R.id.text_instrument)
+    TextView textViewInstrument;
+
     private int mode, instrument, category, opern;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
         Intent intent = getIntent();
         mode = intent.getIntExtra("mode", 0);
         instrument = intent.getIntExtra("instrument", 0);
         category = intent.getIntExtra("category", 0);
+//        TextView textViewOpern = (TextView)findViewById(R.id.text_opern);
+//        textViewOpern.setText("regrewger");
         if (mode == 0) {
+            Log.i("s","mode==0");
+            textViewMode.setText("模式：自由演奏");
+        } else {
+            Log.i("s","mode==1");
             opern = intent.getIntExtra("opern", 0);
+            textViewMode.setText("模式：跟谱演奏");
+            textViewOpern.setText(getResources().getStringArray(R.array.spinner_opern)[opern]);
+        }
+        if (instrument == 0 && category == 0) {
+            Log.i("s","instrument == 0 && category == 0");
+            textViewInstrument.setText("乐器：15键钢琴");
+        } else if (instrument == 0 && category == 1) {
+            Log.i("s","instrument == 0 && category == 1");
+            textViewInstrument.setText("乐器：21键钢琴");
+        } else {
+            Log.i("s","instrument == 1");
+            textViewInstrument.setText("乐器：7孔笛");
         }
         // TODO;
     }

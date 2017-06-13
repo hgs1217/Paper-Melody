f97aa9376277c52161b0d11683951399a4f5863e
YujiePan <panyujie97@126.com>
2017-06-14 06:54:00 +0800

    SettingsPrivacyActivity第一版

--- app/src/main/java/com/papermelody/activity/SettingsPrivacyActivity.java
+++ app/src/main/java/com/papermelody/activity/SettingsPrivacyActivity.java
@@ -1,22 +1,45 @@
 package com.papermelody.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.papermelody.R;
+import com.papermelody.fragment.SettingsPrivacyPreferenceFragment;
 
-/**
- * Created by HgS_1217_ on 2017/4/10.
- */
+import butterknife.BindView;
+
 
 public class SettingsPrivacyActivity extends BaseActivity {
     /**
      * 用例：修改设置（流程二）
      * 账号与隐私设置页面
      */
+    @BindView(R.id.toolbar_settings_play)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initToolbar();
+        initPreferenceMenu();
+    }
+
+    private void initPreferenceMenu() {
+        getFragmentManager().beginTransaction().replace(
+                R.id.frameLayout_settings_privacy, new SettingsPrivacyPreferenceFragment(
+                )).commit();
+    }
+
+
+    private void initToolbar() {
+        toolbar.setTitle(R.string.settings_privacy);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener((View v) -> {
+            finish();
+        });
     }
 
     @Override

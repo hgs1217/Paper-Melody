eb2d5747a06b4c4fedd8b5dfc3b4be67cc476b22
YujiePan <panyujie97@126.com>
2017-06-16 03:03:43 +0800

    超好看的关于界面！
    一定要看！

--- app/src/main/java/com/papermelody/activity/AboutActivity.java
+++ app/src/main/java/com/papermelody/activity/AboutActivity.java
@@ -1,8 +1,14 @@
 package com.papermelody.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 import com.papermelody.R;
+
+import butterknife.BindView;
 
 /**
  * Created by HgS_1217_ on 2017/3/18.
@@ -13,10 +19,19 @@ public class AboutActivity extends BaseActivity {
      * 用例：查看关于
      * 关于页面
      */
-
+    @BindView(R.id.webview)
+    WebView webView;
+@BindView(R.id.toolbar2)
+    Toolbar xx;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        xx.setTitle("Surprise!");
+        WebSettings settings = webView.getSettings();
+        webView.loadUrl("https://yujiepan.github.io/fun.html");
+        settings.setSupportZoom(true);          //支持缩放
+        settings.setBuiltInZoomControls(true);  //启用内置缩放装置
+        settings.setJavaScriptEnabled(true);    //启用JS脚本
     }
 
 

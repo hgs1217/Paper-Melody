a58dab1df4919c48eb3be122acf5f9e2a28f17f2
hgs1217 <mf20080540@126.com>
2017-06-17 15:37:06 +0800

    move the calibration category type sync to calibrationAct

--- app/src/main/java/com/papermelody/activity/PlayActivity.java
+++ app/src/main/java/com/papermelody/activity/PlayActivity.java
@@ -323,7 +323,7 @@ public class PlayActivity extends BaseActivity {
         category = intent.getIntExtra(EXTRA_CATIGORY, 0);
         opernNum = intent.getIntExtra(EXTRA_OPERN, 0);
         opernNum = 1; // FIXME: opern默认
-        calibrationResult = (CalibrationResult) intent.getSerializableExtra(EXTRA_RESULT);
+
         initSoundPool();
         initVoice();
 
@@ -331,14 +331,7 @@ public class PlayActivity extends BaseActivity {
         startBackgroundThread();
         viewPlay.setSurfaceTextureListener(surfaceTextureListener);
 
-
-
-
         Tap.reset();
-
-
-
-
     }
 
     public void processImage(Image image) {
